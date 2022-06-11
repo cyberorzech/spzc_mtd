afb7bc3f5aeabf7a43029eaccaa4e6698d447630
cyberorzech <natan.orze@gmail.com>
2022-06-11 10:32:05 -0400

    MTD works fine. Container info may be exported to file

--- mtd/mtd.py
+++ mtd/mtd.py
@@ -9,17 +9,20 @@ from src.logger import initialize_logger
 from src.container import *
 
 IMAGE_NAME = "webserver"
-SLEEP_INTERVAL = 5 #[s]
+SLEEP_INTERVAL = 10 #[s]
 PUBLISHED_PORTS_RANGE = [5000, 6000]
+CONFIG_PATH = "./exposed_container.json"
 
 def main():
     while(True):
         exposed_port = randint(*PUBLISHED_PORTS_RANGE)
-        container_id = create(image_name=IMAGE_NAME, exposed_port=exposed_port)
-        logger.success(f"Container exposed on port {exposed_port} with id {container_id}")
+        container = create(image_name=IMAGE_NAME, exposed_port=exposed_port)
+        logger.success(f"Container exposed on port {exposed_port} with id {container}")
+        export_container_info(container, exposed_port, CONFIG_PATH)
+        logger.success(f"Config at {CONFIG_PATH} has been updated")
         sleep(SLEEP_INTERVAL)
-        delete_container(container_id)
-        logger.success(f"Deleted container {container_id}")
+        delete_container(container)
+        logger.success(f"Deleted container {container}")
 
 def signal_handler(sig, frame):
     logger.info("Cleaning containers...")
--- mtd/src/container.py
+++ mtd/src/container.py
@@ -1,5 +1,7 @@
 from python_on_whales import docker
 from loguru import logger
+from json import dump
+from datetime import datetime
 
 @logger.catch
 def create(image_name: str, exposed_port: int):
@@ -16,6 +18,16 @@ def delete_container(container_id):
 def delete_active_containers():
     active_containers = docker.ps()
     docker.stop(*active_containers)
+
+@logger.catch
+def export_container_info(container, exposed_port, path="./exposed_container.json"):
+    container_info = {
+        "id": container.id,
+        "exposed_port": exposed_port,
+        "timestamp": datetime.timestamp(datetime.now())
+    }
+    with open(path, "w") as f:
+        dump(container_info, f)
 
 if __name__ == "__main__":
     raise NotImplementedError("Use as package")
