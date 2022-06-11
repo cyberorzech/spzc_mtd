d47d5ec6a18189509213920b3cef2978b85e76bd
cyberorzech <natan.orze@gmail.com>
2022-06-11 11:44:30 -0400

    Implement measuring downtime on client script

--- mtd/client.py
+++ mtd/client.py
@@ -1,5 +1,5 @@
 import requests
-from time import sleep
+from time import sleep, perf_counter
 from loguru import logger
 from json import load
 
@@ -24,19 +24,46 @@ def perform_request(port: int):
     except requests.exceptions.ConnectionError as ce:
         return str(ce)
 
-def main():
+@logger.catch
+def measure_requests():
     success_count = 0
     failure_count = 0
 
     while(True):
         port = get_containers_port()
         response = perform_request(port)
-        if "200" in str(response):
+        if "Response [200]" in str(response):
             success_count += 1
         else:
             failure_count += 1
         logger.success(f"{success_count=}, {failure_count=}")
         # sleep(0.2)
+
+@logger.catch
+def measure_downtime():
+    downtime = 0
+    uptime = 0
+    loop_count = 0
+
+    while(True):
+        start_time = perf_counter()
+        port = get_containers_port()
+        response = perform_request(port)
+        if "Response [200]" in str(response):
+            end_time = perf_counter()
+            uptime += end_time - start_time
+        else:
+            end_time = perf_counter()
+            downtime += end_time - start_time
+        loop_count += 1
+        if loop_count % 1000 == 0:
+            logger.success(f"Uptime: {round(uptime, 2)}[s], Downtime: {round(downtime, 2)}[s]")
+            loop_count = 0
+
+
+def main():
+    #measure_requests()
+    measure_downtime()
 
 if __name__ == "__main__":
     initialize_logger(LOG_FILENAME)
